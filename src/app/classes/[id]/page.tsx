27da38bb70ef19bd8b7eac57c3bd88bdf14db98e
TarTheesaran <tar.theesaran@odds.team>
2024-03-31 11:14:48 +0700

    feature course detail, html skeleton

--- src/app/classes/[id]/page.tsx
+++ src/app/classes/[id]/page.tsx
@@ -1,3 +1,42 @@
 export default function ClassDetail() {
-  return <div>Test Class Detail</div>;
+  return <div className="h-max">
+  <div>
+  <div>
+      <h1>[Class name]</h1>
+      <h2>Class detail</h2>
+      <p>some thing some thing some thing some thing</p>
+      <p>
+        <span>เปิดรับ:</span>
+        <span>20 คน</span>
+        <span>category name</span>
+      </p>
+      <p>
+        <span>วันที่เรียน: วันจันทร์ ที่ 1 มกราคม 2564 - วันศุกร์ ที่ 31 ธันวาคม 2564</span>
+        <span> </span>
+        <span>สถานที่เรียน: </span>
+        <a href="www.google.com" >link</a>
+      </p>
+    </div>
+    <div>
+      <h2>สิ่งที่ต้องเตรียม</h2>
+      <p>some thing some thing some thing some thing </p>
+    </div>
+  </div>
+<div>
+  <h2>Instructor</h2>
+  <div>
+      <div>
+        <img src="https://via.placeholder.com/150" alt="instructor name" />
+        <div>
+          <h3>[Instructor name]</h3>
+          <p>some thing some thing some thing some thing</p>
+          <h3>
+            <p>experience</p>
+            <p>some thing some thing</p>
+          </h3>
+        </div>
+      </div>
+  </div>
+</div>
+  </div>
 }
