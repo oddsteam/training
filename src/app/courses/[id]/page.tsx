5b80e17bca74bf64907366fa35320e511a65dc74
nawajar <nawapb@gmail.com>
2024-03-31 14:12:50 +0700

    add type for course detail

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -5,7 +5,9 @@ import { useEffect, useState } from "react";
 
 export type CourseDetail = {
   courseName: string;
+  outline: string;
   images: string[];
+  duration: string;
 };
 
 export default function CourseDetail({ params }: { params: { id: string } }) {
@@ -36,10 +38,21 @@ export default function CourseDetail({ params }: { params: { id: string } }) {
       <div className="flex w-full justify-between p-4 gap-2 bg-white">
         <div className="flex flex-col w-2/3 bg-white rounded">
           <div className="w-full flex m-4">
-            <div className="mb-4 w-1/2">
-              <p className="font-bold">{data?.courseName}</p>
-              <p className="font-medium"> This is detail course xxxxxx.</p>
+            <div className="w-1/2">
+              <div className="m-4">
+                <p className="font-bold">Course Name</p>
+                <p className="font-medium"> {data?.courseName}</p>
+              </div>
+              <div className="m-4">
+                <p className="font-bold">Course Outline</p>
+                <p className="font-medium"> {data?.outline}</p>
+              </div>
+              <div className="m-4">
+                <p className="font-bold">Durations</p>
+                <p className="font-medium"> {data?.duration} days</p>
+              </div>
             </div>
+
             <div className="m-4 w-1/2 flex flex-wrap gap-2">
               {data?.images.map((image) => (
                 <Image
@@ -52,14 +65,6 @@ export default function CourseDetail({ params }: { params: { id: string } }) {
                 />
               ))}
             </div>
-          </div>
-          <div className="m-4">
-            <h1>Outline</h1>
-            <p> This is course outline.</p>
-          </div>
-          <div className="m-4">
-            <h1>Outline</h1>
-            <p> This is course outline.</p>
           </div>
         </div>
         <div className="mb-4 w-1/3 rounded flex flex-col">
