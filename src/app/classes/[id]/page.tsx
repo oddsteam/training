0ecb979c6e77d60e4d7a794c0eee7e6daf4892be
TarTheesaran <tar.theesaran@odds.team>
2024-03-31 13:54:45 +0700

    update class detail layout

--- src/app/classes/[id]/page.tsx
+++ src/app/classes/[id]/page.tsx
@@ -3,47 +3,60 @@ export default async function ClassDetail({ params }: { params: { id: string } }
   console.log(classDetail);
   const classRemaing = await getClassRemaing(params.id)
   console.log(classRemaing);
-
-return <div className="h-max">
-  <div>
-  <div>
-      <h1>[{classDetail.name}]</h1>
-      <h2>Class detail</h2>
-      <p>{classDetail.description}</p>
-      <p>
-        <span>เปิดรับ:</span>
-        <span>{classDetail.maxParticipant} คน</span>
-        <span>category name</span>
-      </p>
-      <p>
-        <span>วันที่เรียน: {classDetail.startDate} - {classDetail.endDate}</span>
+  return <div className="h-max max-w-full">
+    <div className="p-16">
+      <div className="flex flex-row gap-4">
+        <div className="flow flow-col w-1/2 border-solid border-2 border-blue-600 rounded-lg">
+          <p className="text-lg font-bold">{classDetail.name}</p>
+          <p className="text-md font-bold">{classDetail.description}</p>
+          <p> some thing some thing some thing some thing some thing some thing some thing some thing some thing some thing some thing some thing </p>
+          <p>
+            <span>เปิดรับ:</span>
+            <span>{classDetail.maxParticipant} คน</span>
+            <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+            category name
+              </span>
+          </p>
+          <p>
+          <span>วันที่เรียน: {classDetail.startDate} - {classDetail.endDate}</span>
         <span>เวลาที่เรียน: {classDetail.startTime} - {classDetail.endTime}</span>
-        <span> </span>
-        <span>สถานที่เรียน: </span>
-        <a href={classDetail.googleMapUrl} >link</a>
-      </p>
-    </div>
-    <div>
-      <h2>สิ่งที่ต้องเตรียม</h2>
-      <p>{classDetail.prerequisite}</p>
-    </div>
-  </div>
-<div>
-  <h2>Instructor</h2>
-  <div>
-      <div>
-        <img src="https://via.placeholder.com/150" alt="instructor name" />
-        <div>
-          <h3>[Instructor name]</h3>
-          <p>some thing some thing some thing some thing</p>
-          <h3>
-            <p>experience</p>
-            <p>some thing some thing</p>
-          </h3>
+            <span>สถานที่เรียน: </span>
+            <a href="classDetail.googleMapUrl" >link</a>
+          </p>
+        </div>
+        <div className="flex flex-col w-1/2 border-2 border-blue-600 rounded-lg">
+          <p className="text-lg font-bold">สิ่งที่ต้องเตรียม</p>
+          <p>{classDetail.prerequisite}</p>
         </div>
       </div>
+      <div className="mt-4 border-solid border-2 border-blue-600 rounded-lg">
+        <p className="font-bold">Instructor</p>
+        <div className="flex flex-row gap-8 mt">
+        <Instructor />
+        <Instructor />
+        </div>
+      </div>
+      <div className="w-full flex flex-row-reverse items-end">
+        <button type="submit"className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Register
+        </button>
+      </div>
+    </div>
   </div>
-</div>
+}
+
+function Instructor() {
+  return <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <img className="w-80" src="https://via.placeholder.com/150" alt="Sunset in the mountains" />
+    <div className="px-6 py-4"> 
+      <div className="font-bold text-xl mb-2">Roof</div>
+      <p className="text-gray-700 text-base text">
+      Experience
+      </p>
+      <p className="text-gray-700 text-base">
+      Experience
+      </p>
+    </div>
   </div>
 }
 
