4144d9a2a81a363ad729bf7cb7c038cf48302769
Nanthachanok Y <guy@odds.team>
2024-03-31 11:27:48 +0700

    feat: created layout course detail

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -1,3 +1,24 @@
 export default function CourseDetail() {
-  return <div>Test Course Detail</div>;
+  return  (
+    <>
+      <div className="mb-4">
+        <h1>
+            Course Name
+        </h1>
+        <p> This is detail course xxxxxx.</p>
+      </div>
+      <div className="mb-4">
+        <h1>
+            Outline
+        </h1>
+        <p> This is course outline.</p>
+      </div>
+      <div className="mb-4">
+        <h1>
+            Duration
+        </h1>
+        <p> This is course outline.</p>
+      </div>
+    </>
+  )
 }
